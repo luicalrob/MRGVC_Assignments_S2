ced2be3238a95fb730e5cb20a4594ec1063373d6
amartinezsilva <andresmarsil77@gmail.com>
2024-05-23 16:55:52 +0200

    working lab2 in gazebo

--- MultirobotSystems/project/mr_rendezvous_deployment_turtlebot/src/rendezvous_turtlebot.py
+++ MultirobotSystems/project/mr_rendezvous_deployment_turtlebot/src/rendezvous_turtlebot.py
@@ -2,8 +2,8 @@
 import random
 import math
 import rospy
-from mr_rendezvous_deployment.msg import queue_position_plot
-from mr_rendezvous_deployment.srv import gossip_update, gossip_updateResponse
+from mr_rendezvous_deployment_turtlebot.msg import queue_position_plot
+from mr_rendezvous_deployment_turtlebot.srv import gossip_update, gossip_updateResponse
 from geometry_msgs.msg import PoseStamped, Pose, Twist
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
@@ -14,11 +14,16 @@ class RendezvousTurtlebot:
         rospy.init_node("robot")
 
         self.Kp_v = 0.1
-        self.Kp_w = 0.25
+        self.Kp_alpha = 2.0
+        self.Kp_beta = -0.1
+        self.rho = 0.0
+        self.alpha = 0.0
+        self.beta = 0.0
 
         self.pose = Pose()
         self.target_pose = Pose()
         self.error = PoseStamped()
+        self.target_reached = False
         self.velocity = Twist()
 
         rospy.sleep(1.0)
@@ -49,7 +54,7 @@ class RendezvousTurtlebot:
         
         # Create timer to request gossip update every t_local cycles
         self.control_timer = rospy.Timer(rospy.Duration(
-            0.05), self.control_update)
+            0.1), self.control_update)
 
     def read_params(self):
         try:
@@ -82,22 +87,31 @@ class RendezvousTurtlebot:
     
 
     def gossip_update_cb(self, req):
-        
+
+
         res = gossip_updateResponse()
-        
+
+                
         res.x_resp = (req.x_req + self.pose.position.x) / 2.0
         res.y_resp = (req.y_req + self.pose.position.y) / 2.0
 
+        
+        # #Uncomment this to only update gossip after reaching next position
+        # if not self.target_reached:
+        #     #rospy.loginfo(" [rendezvous robot] Robot {} has not reached the target position yet.".format(self.robot_id))
+        #     res.success = False
+        #     return res  # Return an empty response if target not reached
+        
         self.target_pose.position.x = res.x_resp
         self.target_pose.position.y = res.y_resp
+        res.success = True
+
+        if self.formation == 'line':
+            self.target_pose.position.x = self.target_pose.position.x + self.inter_distance_x * (self.robot_id-1)
+            self.target_pose.position.y = self.target_pose.position.y + self.inter_distance_y * (self.robot_id-1)
 
         rospy.loginfo(" [rendezvous robot] Target position of robot {} updated: {}".format(
                 self.robot_id, self.target_pose.position))
-        
-
-        if self.formation == 'line':
-            self.target_pose.position.x = self.target_pose.position.x + self.inter_distance_x * self.robot_id
-            self.target_pose.position.y = self.target_pose.position.y + self.inter_distance_y * self.robot_id
         
         return res
 
@@ -120,16 +134,17 @@ class RendezvousTurtlebot:
             res = service_gossip_update( self.robot_id,
                 self.pose.position.x, self.pose.position.y)
             
+            if(res.success is False): return
+
             self.target_pose.position.x = res.x_resp 
             self.target_pose.position.y = res.y_resp
+
+            if self.formation == 'line':
+                self.target_pose.position.x  = self.target_pose.position.x + self.inter_distance_x * (self.robot_id-1)
+                self.target_pose.position.y  = self.target_pose.position.y + self.inter_distance_y * (self.robot_id-1)
             
             rospy.loginfo("[rendezvous robot] Target position of robot {} updated: {}".format(
                 self.robot_id, self.pose.position))
-
-            if self.formation == 'line':
-                self.target_pose.position.x  = self.target_pose.position.x + self.inter_distance_x * self.robot_id
-                self.target_pose.position.y  = self.target_pose.position.y + self.inter_distance_y * self.robot_id
-            
 
         except rospy.ServiceException as e:
             print("[rendezvous robot] Service call failed: "+str(e))
@@ -137,11 +152,12 @@ class RendezvousTurtlebot:
     #High frequency control callback to send velocity commands
     def control_update(self, event):
 
+        
         # Calculate the heading to the target
         self.error.pose.position.x = self.target_pose.position.x - self.pose.position.x
         self.error.pose.position.y = self.target_pose.position.y - self.pose.position.y
 
-        d = math.sqrt(self.error.pose.position.x*self.error.pose.position.x + self.error.pose.position.y*self.error.pose.position.y)
+        self.rho = math.sqrt(self.error.pose.position.x*self.error.pose.position.x + self.error.pose.position.y*self.error.pose.position.y)
 
         target_angle = math.atan2(self.error.pose.position.y, self.error.pose.position.x)
 
@@ -150,30 +166,38 @@ class RendezvousTurtlebot:
         (_, _, yaw) = euler_from_quaternion([orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w])
 
         # Calculate angular difference
-        self.error.pose.orientation.z = target_angle - yaw
+        self.alpha = target_angle - yaw
+
+        if (self.alpha < -math.pi): self.alpha = 2.0*math.pi - abs(self.alpha)
+        if (self.alpha > math.pi): self.alpha = -2.0*math.pi + self.alpha
+
+        self.beta = - self.alpha - yaw
+        if (self.beta < -math.pi): beta = 2.0*math.pi - abs(self.beta)
+        if (self.beta > math.pi): beta = -2.0*math.pi + self.beta
 
         # Normalize the angular difference
-        self.error.pose.orientation.z= math.atan2(math.sin(self.error.pose.orientation.z), math.cos(self.error.pose.orientation.z))
-
-        if (d < 0.1 and abs(self.error.pose.orientation.z) < 0.1): return
+        self.error.pose.orientation.z= self.alpha
 
         #Publish error for debug
         self.error.header.stamp = rospy.Time.now()
         self.error_publisher.publish(self.error)
+
+        self.target_reached = self.rho < 0.2
+
+        if self.target_reached: return
 
         self.command_velocity()
 
     def command_velocity(self):
         
         # Calculate linear velocity in x
-        self.velocity.linear.x = self.Kp_v * self.error.pose.position.x
+        self.velocity.linear.x = self.Kp_v * self.rho
 
         # Calculate angular velocity in z
-        self.velocity.angular.z = self.Kp_w * self.error.pose.orientation.z
+        self.velocity.angular.z = self.Kp_alpha * self.alpha + self.Kp_beta * self.beta
 
         # Publish the velocity command
         self.velocity_publisher.publish(self.velocity)
-
 
 
 
