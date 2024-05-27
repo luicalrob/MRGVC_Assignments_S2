6e760a07bd808c8e12feb76308ed5b8619ceecd1
luicalrob <luiscalderonr00@gmail.com>
2024-05-27 18:07:36 +0200

    change interfaz scripts

--- MultirobotSystems/project/mr_formation/src/interfaz.py
+++ MultirobotSystems/project/mr_formation/src/interfaz.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-from lib_interface import *
-
+from lib_interfaz import *
 from geometry_msgs.msg import Polygon, Point32
 import rospy
 import numpy as np 
@@ -9,127 +8,121 @@ import pygame, sys
 
 # Initializing ros node
 rospy.init_node("interface_node")
+num_robots = int(rospy.get_param("~n_robots"))
 
-#read number of robots
-n_robots = int(rospy.get_param("~n_robots"))
-rospy.loginfo("[Interface] Launching pattern drawing interface for {} robots".format(n_robots))
+# Publisher for sending goal points
+goal_publisher = rospy.Publisher('/goal_points', Polygon, queue_size=5)
 
-sol_pub = rospy.Publisher('/goal_points', Polygon, queue_size=5)
-desired_r_pubs = [rospy.Publisher(f'/robot_{i}/desired_r', Point32, queue_size=10) for i in range(1, n_robots+1)]
+# Publishers for sending desired positions to each turtle
+desired_pos_publishers = [rospy.Publisher(f'/robot_{i}/desired_r', Point32, queue_size=10) for i in range(1, n_robots+1)]
 
 # Initializing pygame
 pygame.init()
 
 # Initializing the main window
 screen = pygame.display.set_mode(WINDOW_SIZE)
-pygame.display.set_caption(config_caption)
-pygame.display.set_icon(pygame.image.load(config_icon))
+pygame.display.set_caption(interface_caption)
+pygame.display.set_icon(pygame.image.load(interface_icon))
 
 # Creating the panel for the buttons
 panel   = pygame.Rect(PANEL_L, PANEL_T, PANEL_WIDTH, PANEL_HEIGHT)
 border  = pygame.Rect(BORDER_L,BORDER_T,BORDER_WIDTH,BORDER_HEIGHT)
 
 # Initializing the form and titles of the main window
-title_t = pygame.image.load(config_title).get_rect()
-pt_rb_t = pygame.image.load(config_pt_rbt).get_rect()
-info_t  = pygame.image.load(config_info).get_rect()
-coord_s = pygame.image.load(config_axes).get_rect()
+title_rect = pygame.image.load(interface_title).get_rect()
+robot_title_rect = pygame.image.load(interface_robot_title).get_rect()
+info_rect  = pygame.image.load(interface_info).get_rect()
+coordinate_rect = pygame.image.load(interface_axes).get_rect()
 
-# Creating the buttons to use
-btn_delete  = Button("BORRAR",  190,50,(15,520), 3,pygame.font.Font(None,20),superlightgrey,grey)
-btn_draw    = Button("DIBUJAR", 190,50,(230,520),3,pygame.font.Font(None,20),superlightgrey,grey)
-btn_send    = Button("ENVIAR",  190,50,(445,520),3,pygame.font.Font(None,20),superlightgrey,grey)
+# Creating the buttons
+btn_delete  = Button("REMOVE",  190,50,(15,520), 3,pygame.font.Font(None,20),superlightgrey,grey)
+btn_draw    = Button("DRAW", 190,50,(230,520),3,pygame.font.Font(None,20),superlightgrey,grey)
+btn_send    = Button("SEND",  190,50,(445,520),3,pygame.font.Font(None,20),superlightgrey,grey)
 
 # Declaration and definition of the functions of the buttons and others
-def fill_super_grid():
-    global super_grid
+def initialize_grid():
+    global grid_state
     for i in range(0,SUPER_GRID_WIDTH):
-        aux = []
+        row = []
         for j in range(0,SUPER_GRID_HEIGHT):
-            aux.append(False)
-        super_grid.append(aux)
+            row.append(False)
+        grid_state.append(row)
 
-def clean_mat():
-    global super_grid,sol,dic
-    for i in range(len(super_grid)):
-        for j in range(len(super_grid[i])):
-            super_grid[i][j] = False
-    sol = []
-    dic = {}
+def clear_grid():
+    global grid_state, solution, grid_dict
+    for i in range(len(grid_state)):
+        for j in range(len(grid_state[i])):
+            grid_state[i][j] = False
+    solution = []
+    grid_dict = {}
 
-def print_path():
-    global sol
+def draw_solution():
+    global solution
     global path
-    global dic
+    global grid_dict
 
-    # Preprocessing of the dic list
-    k = list(dic.keys())
-    # print(k)
-    keys = []
-    for i in k:
+    # Preprocessing of the grid dictionary
+    keys = list(grid_dict.keys())
+    key_points = []
+    for i in keys:
         x = int(i/100)
         y = i%100
-        keys.append(Point(x,y))
+        key_points.append(Point(x,y))
     
-    for _k in keys:
-        print(_k.x, _k.y)
+    for k in key_points:
+        print(k.x, k.y)
 
-    # print(len(path))
-    sol = []
-    if len(keys) <= n_robots:
-        sol = keys.copy() 
+    solution = []
+    if len(key_points) <= num_robots:
+        solution = key_points.copy() 
     else:
-        i = aux = (len(keys) - (len(keys) / n_robots)) / (n_robots - 2)
-        sol.append(keys[0])
-        while(i <= len(keys)):
+        i = aux = (len(key_points) - (len(key_points) / num_robots)) / (num_robots - 2)
+        solution.append(key_points[0])
+        while(i <= len(key_points)):
             aux2 = int(i)
-            sol.append(keys[aux2])
+            solution.append(key_points[aux2])
             i = i + aux
-        sol.append(keys[-1])
+        solution.append(key_points[-1])
         
     print("-----")
-    print(len(sol), len(keys), len(path))
-    for i in sol:
+    print(len(solution), len(key_points), len(path))
+    for i in solution:
         print(i.x, i.y)
 
-def send_sol():
-
+def send_solution():
     poly = Polygon()
-    for s in sol:
+    for s in solution:
         poly.points.append(Point32(s.y*2/10,s.x*2/10,0))
         
-    sol_pub.publish(poly)
+    goal_publisher.publish(poly)
 
     # Calculate relative positions
-    num_goals = len(sol)
-    desired_r = np.zeros((num_goals,2))
+    num_goals = len(solution)
+    desired_pos = np.zeros((num_goals,2))
     rel_pos_matrix = np.zeros((num_goals, num_goals, 2))
     
     for i in range(num_goals):
         for j in range(num_goals):
             if i != j:
-                rel_pos_matrix[i, j, 0] = (sol[j].x - sol[i].x) * 2/10
-                rel_pos_matrix[i, j, 1] = (sol[j].y - sol[i].y) * 2/10
+                rel_pos_matrix[i, j, 0] = (solution[j].x - solution[i].x) * 2/10
+                rel_pos_matrix[i, j, 1] = (solution[j].y - solution[i].y) * 2/10
         
-        desired_r[i,0] = -np.sum(rel_pos_matrix[i,:,0])
-        desired_r[i,1] = -np.sum(rel_pos_matrix[i,:,1])
-        desired_r_pubs[i].publish(Point32(desired_r[i,0], desired_r[i,1], 0))
+        desired_pos[i,0] = -np.sum(rel_pos_matrix[i,:,0])
+        desired_pos[i,1] = -np.sum(rel_pos_matrix[i,:,1])
+        desired_pos_publishers[i].publish(Point32(desired_pos[i,0], desired_pos[i,1], 0))
                 
     rospy.loginfo("Desired relative positions in x: {}".format(rel_pos_matrix[:,:,0]))
     rospy.loginfo("Desired relative positions in y: {}".format(rel_pos_matrix[:,:,1]))
 
 # Initializing the solution grid
-fill_super_grid()
-
+initialize_grid()
 
 # Infinite loop where the screen updates
 while True:
-
     # First, check the event list
     for event in pygame.event.get():
         # Check if there's any QUIT event
         if event.type == pygame.QUIT: sys.exit()
-    
 
     # Fill screen with background color, panel for buttons and border
     screen.fill(superlightgrey)
@@ -137,59 +130,58 @@ while True:
     pygame.draw.rect(screen,lightgrey,border,BORDER_BORDER_WIDTH)
 
     # Placing the titles
-    title_t.x, title_t.y    =   65, 10
-    pt_rb_t.x, pt_rb_t.y    =   70, 55
-    info_t.x,  info_t.y     =   20, 465
-    coord_s.x, coord_s.y    =   -2, -2
-    screen.blit(pygame.image.load(config_title), title_t)
-    screen.blit(pygame.image.load(config_pt_rbt),pt_rb_t)
-    screen.blit(pygame.image.load(config_info),  info_t)
-    screen.blit(pygame.image.load(config_axes),coord_s)
+    title_rect.x, title_rect.y    =   65, 10
+    robot_title_rect.x, robot_title_rect.y    =   70, 55
+    info_rect.x,  info_rect.y     =   20, 465
+    coordinate_rect.x, coordinate_rect.y    =   -2, -2
+    screen.blit(pygame.image.load(interface_title), title_rect)
+    screen.blit(pygame.image.load(interface_robot_title),robot_title_rect)
+    screen.blit(pygame.image.load(interface_info),  info_rect)
+    screen.blit(pygame.image.load(interface_axes),coordinate_rect)
     
     # Fill the screen with the widgets and configuration
-    btn_delete.draw(screen, clean_mat)
-    btn_draw.draw(screen, print_path)
-    btn_send.draw(screen, send_sol)
+    btn_delete.draw(screen, clear_grid)
+    btn_draw.draw(screen, draw_solution)
+    btn_send.draw(screen, send_solution)
 
     # Drawing the grid
     for i in range(GRID_L,GRID_W,CELL_W):
         for j in range(GRID_T,GRID_H,CELL_H):
             # Drawing the main grid
-            grid = pygame.Rect(i,j,CELL_W,CELL_H)
-            pygame.draw.rect(screen,lightgrey, grid,CELL_BORDER_WIDTH)
+            grid_rect = pygame.Rect(i,j,CELL_W,CELL_H)
+            pygame.draw.rect(screen,lightgrey, grid_rect,CELL_BORDER_WIDTH)
 
-            # Getting the mouse events in order to draw or delete the path - this path is saved at super_grid
-            izq = pygame.mouse.get_pressed()[0]
-            dch = pygame.mouse.get_pressed()[2] 
-            if izq or dch:
+            # Getting the mouse events in order to draw or delete the path - this path is saved at grid_state
+            left_click = pygame.mouse.get_pressed()[0]
+            right_click = pygame.mouse.get_pressed()[2] 
+            if left_click or right_click:
                 x,y = pygame.mouse.get_pos()
                 if x > i and x < i+CELL_W and y > j and y < j+CELL_H:
-                    if izq:                  
-                        super_grid[int((i-GRID_L)/CELL_W)][int((j-GRID_T)/CELL_H)] = True
-                        dic.update({int((i-GRID_L)/CELL_W)*100 + int((j-GRID_T)/CELL_H):True})
-                    if dch:
-                        super_grid[int((i-GRID_L)/CELL_W)][int((j-GRID_T)/CELL_H)] = False
+                    if left_click:                  
+                        grid_state[int((i-GRID_L)/CELL_W)][int((j-GRID_T)/CELL_H)] = True
+                        grid_dict.update({int((i-GRID_L)/CELL_W)*100 + int((j-GRID_T)/CELL_H):True})
+                    if right_click:
+                        grid_state[int((i-GRID_L)/CELL_W)][int((j-GRID_T)/CELL_H)] = False
                         try:
-                            dic.pop(int((i-GRID_L)/CELL_W)*100 + int((j-GRID_T)/CELL_H))
+                            grid_dict.pop(int((i-GRID_L)/CELL_W)*100 + int((j-GRID_T)/CELL_H))
                         except KeyError:
                             continue
-                        sol = []
-    
+                        solution = []
+
     # Drawing the selected grids (Done when right button of the mouse is pressed)
     path = []
     for i in range(0,SUPER_GRID_WIDTH):
         for j in range(0,SUPER_GRID_HEIGHT):
-            if super_grid[i][j] == True: 
+            if grid_state[i][j] == True: 
                 rect = pygame.Rect(i*CELL_W+GRID_L,j*CELL_H+GRID_T,CELL_W,CELL_W)
                 pygame.draw.rect(screen,white,rect,0)
                 path.append(Point(i,j))
 
-    # Drawing the 10 points (in case it had been selected 10 or more points on the grid) 
-    # where the robots will place ()
-    for s in sol:
+    # Drawing the points where the robots will move
+    for s in solution:
         rect = pygame.Rect(s.x*CELL_W+GRID_L,s.y*CELL_H+GRID_T,CELL_W,CELL_H)
         pygame.draw.rect(screen,lightblue,rect,0)
-
+    
     mx, my = pygame.mouse.get_pos()
     if mx > 20 and mx < 35 and my > 465 and my < 480:
         info_panel = pygame.Rect(25, 330, 520, 115)
@@ -199,24 +191,20 @@ while True:
         pygame.draw.polygon(screen, light_white, triangle_points, 0)
 
         for t in range(len(info_text)):
-            our_font = pygame.font.Font(current_file_path + "/fonts/ARCADE_R.TTF",8)
+            font = pygame.font.Font(current_file_path + "/fonts/ARCADE_R.TTF",8)
             if t == 0:
 
-                our_font.set_underline(True)
-                our_font_surface = our_font.render(info_text[t],True,'#000000')
-                our_font_text_rect = our_font_surface.get_rect()
-                our_font_text_rect.x, our_font_text_rect.y = 200, 336 + t * 20
+                font.set_underline(True)
+                font_surface = font.render(info_text[t],True,'#000000')
+                font_text_rect = font_surface.get_rect()
+                font_text_rect.x, font_text_rect.y = 200, 336 + t * 20
 
             else:
 
-                our_font_surface = our_font.render(info_text[t],True,'#000000')
-                our_font_text_rect = our_font_surface.get_rect()
-                our_font_text_rect.x, our_font_text_rect.y = 29, 336 + t * 20
+                font_surface = font.render(info_text[t],True,'#000000')
+                font_text_rect = font_surface.get_rect()
+                font_text_rect.x, font_text_rect.y = 29, 336 + t * 20
 
-            screen.blit(our_font_surface, our_font_text_rect)
-
-
-        
-
+            screen.blit(font_surface, font_text_rect)
     # Updating the app
     pygame.display.flip()
---
+++ MultirobotSystems/project/mr_formation/src/lib_interfaz.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+# Importing necessary libraries
+import pygame
+import os
+
+# Path to current file directory
+current_file_path = os.path.dirname(os.path.abspath(__file__))
+
+# Window and grid dimensions
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = [650,600]
+GRID_SIZE   = GRID_WIDTH,  GRID_HEIGHT    = [600,400]
+
+# Panel dimensions and position
+PANEL_L             = 0
+PANEL_T             = 500
+PANEL_WIDTH         = WINDOW_WIDTH
+PANEL_HEIGHT        = WINDOW_HEIGHT - PANEL_T
+PANEL_BORDER_WIDTH  = 0
+
+# Border dimensions and position
+BORDER_L            = 0
+BORDER_T            = 0
+BORDER_WIDTH        = WINDOW_WIDTH
+BORDER_HEIGHT       = WINDOW_HEIGHT - PANEL_HEIGHT
+BORDER_BORDER_WIDTH = 15
+
+# Cell dimensions and border width
+CELL_W              = 10
+CELL_H              = 10
+CELL_BORDER_WIDTH   = 1
+
+# Grid position and size
+GRID_L              = 75 #125
+GRID_W              = 575 #525
+GRID_T              = 80
+GRID_H              = 480
+
+# Super grid dimensions
+SUPER_GRID_WIDTH    = 50
+SUPER_GRID_HEIGHT   = 40
+
+# Number of turtles
+NUM_TURTLES = 10
+
+# Configuration files paths
+config_loc      = current_file_path + "/config/icons/"
+interface_caption  = "Formation creation"
+interface_icon     = config_loc + "shapingpattern_icon.png"
+interface_title    = config_loc + "title.png"
+interface_robot_title   = config_loc + "points_and_robots.png"
+interface_info     = config_loc + "info.png"
+interface_axes     = config_loc + "coordinate_system.png"
+
+# Color definitions
+white           = [255,255,255]
+light_white     = [200,200,200]
+superlightgrey  = [50,50,50]
+lightgrey       = [30,25,25]
+grey            = [18,18,18]
+black           = [0,0,0]
+lightblue       = [100,100,255]
+
+# Variables for grid state, solution, and dictionary
+grid_state      = []
+path            = []
+solution        = []
+grid_dict       = {}
+
+# Information text
+info_text       = ["Usage Information:", 
+                    "Left-click to draw",  
+                    "Right-click to erase", 
+                    "Press 'Draw' to generate turtle points", 
+                    "Press 'Send' to start turtle movement"]
+
+# Point class definition
+class Point: 
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# Button class definition
+class Button:
+    def __init__(self, text, width, height, pos, elevation, font, color_on, color_off):
+        # Core attributes 
+        self.pressed = False
+        self.elevation = elevation
+        self.dynamic_elevation = elevation
+        self.original_y_pos = pos[1]
+        self.color_on = color_on
+        self.color_off = color_off
+
+        # Top rectangle 
+        self.top_rect = pygame.Rect(pos,(width,height))
+        self.top_color = self.color_off
+
+        # Bottom rectangle 
+        self.bottom_rect = pygame.Rect(pos,(width,height))
+        self.bottom_color = '#354B5E'
+
+        # Text
+        self.text_surf = font.render(text,True,'#FFFFFF')
+        self.text_rect = self.text_surf.get_rect(center=self.top_rect.center)
+
+    def draw(self, screen, function):
+        # Elevation logic 
+        self.top_rect.y = self.original_y_pos - self.dynamic_elevation
+        self.text_rect.center = self.top_rect.center 
+
+        self.bottom_rect.midtop = self.top_rect.midtop
+        self.bottom_rect.height = self.top_rect.height + self.dynamic_elevation
+
+        pygame.draw.rect(screen, self.bottom_color, self.bottom_rect, border_radius=12)
+        pygame.draw.rect(screen, self.top_color, self.top_rect, border_radius=12)
+        screen.blit(self.text_surf, self.text_rect)
+        self.check_click(function)
+
+    def check_click(self, function):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.top_rect.collidepoint(mouse_pos):
+            self.top_color = self.color_on
+            if pygame.mouse.get_pressed()[0]:
+                self.dynamic_elevation = 0
+                self.pressed = True
+            else:
+                self.dynamic_elevation = self.elevation
+                if self.pressed == True:
+                    function()
+                    self.pressed = False
+        else:
+            self.dynamic_elevation = self.elevation
+            self.top_color = self.color_off
